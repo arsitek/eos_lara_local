230eec2f51c038032e9e80ad67249d9daece4c1e
arsitek <chandra.ghv@gmail.com>
2026-08-21 22:11:08 +0700

    Perbaiki query dataBackup untuk menampilkan bulan dan tahun di halaman dayaserap

--- app/Http/Controllers/Laporan/DayaSerapController.php
+++ app/Http/Controllers/Laporan/DayaSerapController.php
@@ -3,63 +3,72 @@
 namespace App\Http\Controllers\Laporan;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Alokasi;
 use App\Models\Datamaster\DuplikasiRkat;
 use Illuminate\Contracts\View\View as ViewContract;
-use \Illuminate\Http\JsonResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
-class DayaSerapController extends Controller {
-    public function index(): ViewContract {
-        [ "tahun" => $tahun, "tahunAngka" => $tahunAngka] = getTahunData(); // from Helper/rekat.php
-        $dataBackup = DuplikasiRkat::select("id", "keterangan")->where(["tahun" => $tahun])->where("keterangan", "LIKE", '%'.$tahunAngka.'%')->get();
+class DayaSerapController extends Controller
+{
+    public function index(): ViewContract
+    {
+        ['tahun' => $tahun, 'tahunAngka' => $tahunAngka] = getTahunData(); // from Helper/rekat.php
+        $dataBackup = DuplikasiRkat::select('id', 'keterangan')->where('keterangan', 'LIKE', '%'.$tahunAngka.'%')->get();
+
         return view('content.laporan.DAYASERAP.index', compact('dataBackup'));
     }
-    public function getAlokasiData(): JsonResponse {
+
+    public function getAlokasiData(): JsonResponse
+    {
         try {
-            [ "tahun" => $tahun, "tahunAngka" => $tahunAngka ] = getTahunData();
-            $alokasi    = DB::connection('sirekat')->select("SELECT sd.sumberdana, alokasi.kd_sumberdana, alokasi.unit_kerja, unit.nama, COALESCE(alokasi.pagu, 0) + COALESCE(alokasi.pagu_tambahan, 0) AS pagu
+            ['tahun' => $tahun, 'tahunAngka' => $tahunAngka] = getTahunData();
+            $alokasi = DB::connection('sirekat')->select("SELECT sd.sumberdana, alokasi.kd_sumberdana, alokasi.unit_kerja, unit.nama, COALESCE(alokasi.pagu, 0) + COALESCE(alokasi.pagu_tambahan, 0) AS pagu
                 FROM tb_alokasi alokasi
                 INNER JOIN tb_sumberdana sd ON sd.kd_sumberdana = alokasi.kd_sumberdana AND sd.is_show = 'true' AND sd.is_deleted = 'false' AND sd.tahun = ?
                 INNER JOIN tb_unit_api unit ON unit.idunit = alokasi.unit_kerja
-                WHERE alokasi.is_deleted = 'false' AND alokasi.tahun = ? ORDER BY sd.kd_sumberdana, unit.idunit", [ $tahunAngka, $tahun ]);
-            $realisasi  = getRekapRealisasi($tahun, "unit");
+                WHERE alokasi.is_deleted = 'false' AND alokasi.tahun = ? ORDER BY sd.kd_sumberdana, unit.idunit", [$tahunAngka, $tahun]);
+            $realisasi = getRekapRealisasi($tahun, 'unit');
+
             return response()->json([
-                "success"    => true,
-                "message"   => "Berhasil mendapatkan data",
-                "data"      => [
-                    "alokasi"   => $alokasi,
-                    "realisasi" => $realisasi
-                ]
+                'success' => true,
+                'message' => 'Berhasil mendapatkan data',
+                'data' => [
+                    'alokasi' => $alokasi,
+                    'realisasi' => $realisasi,
+                ],
             ]);
-        } catch ( \Exception $e ) {
+        } catch (\Exception $e) {
             return response()->json([
-                "success"    => false,
-                "message"   => "Gagal mendapatkan data",
-                "error"     => $e->getMessage()
+                'success' => false,
+                'message' => 'Gagal mendapatkan data',
+                'error' => $e->getMessage(),
             ], 500);
         }
     }
-    public function getAlokasiBackup( Request $req ): JsonResponse {
+
+    public function getAlokasiBackup(Request $req): JsonResponse
+    {
         try {
-            [ "tahun" => $tahun, "tahunAngka" => $tahunAngka] = getTahunData();
+            ['tahun' => $tahun, 'tahunAngka' => $tahunAngka] = getTahunData();
             $validated = Validator::make($req->all(), [
-                'idBackup' => 'required|integer|exists:tb_duplikasi_rkat,id'
-            ],[
+                'idBackup' => 'required|integer|exists:tb_duplikasi_rkat,id',
+            ], [
                 'idBackup.required' => 'ID Backup harus diisi',
-                'idBackup.integer'  => 'ID Backup harus berupa angka',
-                'idBackup.exists'   => 'ID Backup tidak ditemukan'
+                'idBackup.integer' => 'ID Backup harus berupa angka',
+                'idBackup.exists' => 'ID Backup tidak ditemukan',
             ]);
-            if ( $validated->fails() )
-                return response()->json([ "success" => false, "message"   => "Validasi gagal", "errors" => $validated->errors() ], 422);
-            $idBackup      = $req->idBackup;
+            if ($validated->fails()) {
+                return response()->json(['success' => false, 'message' => 'Validasi gagal', 'errors' => $validated->errors()], 422);
+            }
+            $idBackup = $req->idBackup;
             $alokasiBackup = DB::connection('sirekat')->select("SELECT sd.sumberdana, ba.*, unit.nama FROM tb_backup_alokasi ba
                 INNER JOIN tb_sumberdana sd ON sd.kd_sumberdana = ba.kode_sd AND sd.is_show = 'true' AND sd.is_deleted = 'false' AND sd.tahun = ?
                 INNER JOIN tb_unit_api unit ON unit.idunit = ba.idunit WHERE ba.id_duplikasi = ? ORDER BY sd.kd_sumberdana, ba.idunit",
-            [ $tahunAngka, $idBackup ]);
-            $query = " SELECT unit.nama AS nama_unit, unit.idunit AS unit_kerja_rkt,
+                [$tahunAngka, $idBackup]);
+            $query = ' SELECT unit.nama AS nama_unit, unit.idunit AS unit_kerja_rkt,
                         sd.kd_sumberdana, sd.sumberdana,
                         SUM( COALESCE(backupRkatDet.jumlah_amprahan, 0) + COALESCE(backupRkatDet.jumlah_realisasi, 0) ) AS jumlah_amprah
                     FROM tb_backup_rkat backupRkat
@@ -68,21 +77,22 @@ class DayaSerapController extends Controller {
                     INNER JOIN tb_unit_api unit ON unit.idunit = backupRkat.idunit
                     WHERE ( backupRkat.id_duplikasi = ? AND backupRkatDet.id_duplikasi = ? )
                 AND backupRkat.tahun = ?
-                GROUP BY unit.idunit, backupRkat.sd";
-            $realisasi = getBaseData( $query, $tahun, $tahunAngka, null, null, [ $idBackup, $idBackup, $tahun ]); // 2 null parameter karena tidak perlu fitler unit dan sumber dana
+                GROUP BY unit.idunit, backupRkat.sd';
+            $realisasi = getBaseData($query, $tahun, $tahunAngka, null, null, [$idBackup, $idBackup, $tahun]); // 2 null parameter karena tidak perlu fitler unit dan sumber dana
+
             return response()->json([
-                "success"    => true,
-                "message"   => "Berhasil mendapatkan data",
-                "data"      => [
-                    "alokasi"   => $alokasiBackup,
-                    "realisasi" => $realisasi,
-                ]
+                'success' => true,
+                'message' => 'Berhasil mendapatkan data',
+                'data' => [
+                    'alokasi' => $alokasiBackup,
+                    'realisasi' => $realisasi,
+                ],
             ]);
-        } catch ( \Exception $e ) {
+        } catch (\Exception $e) {
             return response()->json([
-                "success" => false,
-                "message" => "Gagal mendapatkan data",
-                "error"   => $e->getMessage()
+                'success' => false,
+                'message' => 'Gagal mendapatkan data',
+                'error' => $e->getMessage(),
             ], 500);
         }
     }
