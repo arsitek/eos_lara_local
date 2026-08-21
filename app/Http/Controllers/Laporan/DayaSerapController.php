e29e5c8646d320ef089f2583b7567b03473dad09
arsitek <chandra.ghv@gmail.com>
2026-08-21 22:30:06 +0700

    Perbaiki nama tabel baseDataBackup menjadi basedatabackup di query getAlokasiBackup

--- app/Http/Controllers/Laporan/DayaSerapController.php
+++ app/Http/Controllers/Laporan/DayaSerapController.php
@@ -63,21 +63,31 @@ class DayaSerapController extends Controller
                 return response()->json(['success' => false, 'message' => 'Validasi gagal', 'errors' => $validated->errors()], 422);
             }
             $idBackup = $req->idBackup;
+
+            // Debug logging
+            Log::info('getAlokasiBackup - idBackup: '.$idBackup);
+            Log::info('getAlokasiBackup - tahun: '.$tahun.', tahunAngka: '.$tahunAngka);
+
             $alokasiBackup = DB::connection('sirekat')->select("SELECT sd.sumberdana, ba.*, unit.nama FROM tb_backup_alokasi ba
                 INNER JOIN tb_sumberdana sd ON sd.kd_sumberdana = ba.kode_sd AND sd.is_show = 'true' AND sd.is_deleted = 'false' AND sd.tahun = ?
                 INNER JOIN tb_unit_api unit ON unit.idunit = ba.idunit WHERE ba.id_duplikasi = ? ORDER BY sd.kd_sumberdana, ba.idunit",
                 [$tahunAngka, $idBackup]);
+
+            Log::info('getAlokasiBackup - alokasiBackup count: '.count($alokasiBackup));
+
             $query = ' SELECT unit.nama AS nama_unit, unit.idunit AS unit_kerja_rkt,
                         sd.kd_sumberdana, sd.sumberdana,
                         SUM( COALESCE(backupRkatDet.jumlah_amprahan, 0) + COALESCE(backupRkatDet.jumlah_realisasi, 0) ) AS jumlah_amprah
                     FROM tb_backup_rkat backupRkat
-                    INNER JOIN baseDataBackup backupRkatDet ON backupRkatDet.id_rekat = backupRkat.id_rekat
+                    INNER JOIN basedatabackup backupRkatDet ON backupRkatDet.id_rekat = backupRkat.id_rekat
                     INNER JOIN sumberdana sd ON sd.kd_sumberdana = backupRkat.sd
                     INNER JOIN tb_unit_api unit ON unit.idunit = backupRkat.idunit
                     WHERE ( backupRkat.id_duplikasi = ? AND backupRkatDet.id_duplikasi = ? )
                 AND backupRkat.tahun = ?
                 GROUP BY unit.idunit, backupRkat.sd';
             $realisasi = getBaseData($query, $tahun, $tahunAngka, null, null, [$idBackup, $idBackup, $tahun]); // 2 null parameter karena tidak perlu fitler unit dan sumber dana
+
+            Log::info('getAlokasiBackup - realisasi count: '.(is_array($realisasi) ? count($realisasi) : 'not array'));
 
             return response()->json([
                 'success' => true,
